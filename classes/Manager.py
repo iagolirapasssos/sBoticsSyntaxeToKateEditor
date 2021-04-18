7a23a2baf8e7a7f11ecc531b8390b3cc297005c3
unknown <vinicioslugli@gmail.com>
2021-04-17 23:55:48 -0300

    Update HOMEPATH and fix diretory creator class

--- classes/Manager.py
+++ classes/Manager.py
@@ -1,24 +1,20 @@
-import json, os
+import json
 
 class JsonManager():
 	def read(self, _file):
-		if not os.path.exists(_file): return
 		with open(_file, "r", encoding="utf8") as jf:
 			return json.load(jf)
 
 	def write(self, _file, _data):
-		if not os.path.exists(_file): return
 		with open(_file, "w", encoding="utf8") as jf:
 			json.dump(_data, jf, ensure_ascii=False, indent=4)
 
 class FileManager():
 	def read(self, _file):
-		if not os.path.exists(_file): return
 		with open(_file, "r", encoding="utf8") as f:
 			return f.read()
 
 	def write(self, _file, _data):
-		if not os.path.exists(_file): return
 		with open(_file, 'w', encoding='utf-8') as f:
 			f.write(_data)
 		f.close()
